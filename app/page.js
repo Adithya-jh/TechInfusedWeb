255fb1025ff1876eee1a7e937b89ad23371949de
Adithya <jhadith88@gmail.com>
2023-04-15 07:35:25 +0530

    complteed half explore aka team section

--- app/page.js
+++ app/page.js
@@ -19,17 +19,19 @@ const Page = () => (
       <div className="gradient-03 z-0" />
       <Explore />
     </div>
+
     <div className="relative">
       <GetStarted />
       <div className="gradient-04 z-0" />
       <WhatsNew />
     </div>
+
+    <World />
     <div className="relative">
-      <World />
-      <div className="gradient-03 z-0" />
       <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
     </div>
-    <Feedback />
     <Footer />
   </div>
 );
