373e168468d062f0bff1c8317457ef4fbaff517f
Adithya <jhadith88@gmail.com>
2023-04-17 06:15:42 +0530

    complted half get-started

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { fadeIn, staggerContainer } from '../utils/motion';
+import { staggerContainer } from '../utils/motion';
 import { TypingText, TitleText, ExploreCard } from '../components';
 
 import { exploreWorlds } from '../constants';
